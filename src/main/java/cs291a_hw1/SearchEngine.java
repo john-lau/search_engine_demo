3a18093d88f061114fd1894d1012f3004c0f96e5
John Lau <johnlau@umail.ucsb.edu>
2018-01-31 13:28:52 -0800

    TODO: Highlighting queries, implemented simple web service

--- src/main/java/cs291a_hw1/SearchEngine.java
+++ src/main/java/cs291a_hw1/SearchEngine.java
@@ -11,6 +11,8 @@ import org.apache.solr.common.SolrDocumentList;
 
 public class SearchEngine {
 	
+	private static String highlight = "<mark>text</mark>";
+	
 	public static void main(String[] args) throws IOException, SolrServerException {
         SolrClient client = new HttpSolrClient.Builder("http://localhost:8983/solr/trec45").build();
 
@@ -19,16 +21,18 @@ public class SearchEngine {
         
         query.setQuery("title:feb");
 ////        query.addFilterQuery("cat:electronics","store:amazon.com"); this is the filter
-        query.setFields("docid");
+        query.setFields("title"); //change title to argument
         query.setStart(0);
 //        
 //        query.set("title", "title"); this is setting the check list boxes
         
-
+        
+        //TODO: highlight query matches in our result
         QueryResponse response = client.query(query);
         SolrDocumentList results = response.getResults();
         for (int i = 0; i < results.size(); ++i) {
-            System.out.println(results.get(i));
+            String result = results.get(i).toString();
+            
         }
         System.out.print(results.size());;
     }
--- src/main/java/cs291a_hw1/WebService.java
+++ src/main/java/cs291a_hw1/WebService.java
@@ -1,7 +1,28 @@
 package cs291a_hw1;
 
+import static spark.Spark.*;
+
 public class WebService {
-	//  public static void main(String[] args) {
-	//  get("/hello", (req, res) -> "Hello World");
-	//}
+	
+  public static void main(String[] args) {
+	  get("/", (req, res) -> 
+		  "<!DOCTYPE HTML><html>" +
+		  "<form action='/test'>" +
+		  "<label for='query'>Search Query</label>" + 
+		  "<input id='query' name='query' class='query-search' type='text'>" +
+		  "<button type='submit' class='btn'>Search</button>" +
+		  "</form></html>"
+	  );
+	  
+	  get("/test", (req, res) -> {
+		  String query = req.queryParams("query");
+		  return test(Integer.valueOf(query), 4);
+	  });
+  }
+  
+  public static int test(int a, int b) {
+	  return a+b;
+  }
+  
+  
 }
