4a7c758eeaa511ed72405de98a423fb8460e08f8
John Lau <johnlau@umail.ucsb.edu>
2018-01-30 21:55:13 -0800

    initial commit - spark

---
+++ src/main/java/cs291a_hw1/Main.java
@@ -0,0 +1,9 @@
+package cs291a_hw1;
+
+import static spark.Spark.*;
+
+public class Main {
+    public static void main(String[] args) {
+        get("/hello", (req, res) -> "Hello World");
+    }
+}
